c48a9176db03e6f29d5f10015a11e4b3a5cc48e3
fdbva <villela.felipe@gmail.com>
2016-06-21 17:55:05 -0300

    MVC template project created

---
+++ BackEndTest01/BackEndTest01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BackEndTest01.MvcApplication" Language="C#" %>
---
+++ BackEndTest01/BackEndTest01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BackEndTest01.Startup))]
+namespace BackEndTest01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
